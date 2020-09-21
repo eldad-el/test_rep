7ebd21f69beb039962995ddfc1b1f6a3fd9fd23c
Eldad Livschitz <eldad.el1234@gmail.com>
2020-09-21 19:57:17 +0300

    test file commit after saved change

--- test.py
+++ test.py
@@ -7,5 +7,5 @@ wit_dir_str = wit_dir.__str__()
 
 files_tree = {}
 
-for directory, folders, files in os.walk(wit_dir):
-    files_tree[Path(directory).parent()] = 
+#for directory, folders, files in os.walk(wit_dir):
+#    files_tree[Path(directory).parent()] = 
